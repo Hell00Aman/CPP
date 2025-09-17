46abbf39c89b722c5ab8ebf509dc55f0fd6c1237
AMAN RAJ <amantk2205@gmail.com>
2025-09-17 21:49:41 +0530

    Create variable.cpp
    This C++ program declares two integers x and y, assigns them values 2 and 5, then prints their values. It uses cout for output, endl to move to the next line, and ends with return 0 to indicate successful program execution.

---
+++ Basics/variable.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>  // Includes all standard C++ libraries
+using namespace std;      // Use standard namespace
+
+int main() {
+    int x, y;   // Variable declaration
+    x = 2;      // Assign value 2 to x
+    y = 5;      // Assign value 5 to y
+
+    cout << x << endl;  // Print value of x and move to next line
+    cout << y;          // Print value of y
+
+    return 0;  // Exit program successfully
+}
