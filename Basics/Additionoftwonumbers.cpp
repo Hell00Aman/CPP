00b250ff7f7b157ff387f9307ca2f3e23f3b9c40
AMAN RAJ <amantk2205@gmail.com>
2025-09-22 11:31:16 +0530

    Create Additionoftwonumbers.cpp
    This C++ program takes two integer inputs from the user, adds them, and displays the result. It begins by including the standard library <bits/stdc++.h> and using the std namespace. Inside main(), two integers x and y are declared. The program reads values using cin, computes their sum, and outputs it with cout, then terminates successfully.

---
+++ Basics/Additionoftwonumbers.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>   // Header file includes all standard libraries
+using namespace std;     // Using standard namespace
+
+int main(){
+    int x,y;             // Declare two integer variables
+    cin >> x >> y;       // Input values for x and y
+    cout << x+y;         // Output the sum of x and y
+    return 0;            // Exit program
+}
