58e5605883b6be5e7daf874e29ceb0c20b84844f
AMAN RAJ <amantk2205@gmail.com>
2025-10-02 17:08:26 +0530

    Create Alleven.cpp
    The program prints all even numbers from 2 to 100 using a for loop. The loop starts at 2 and increments by 2 in each iteration, ensuring only even numbers are selected. Each even number is displayed on a new line using cout. This demonstrates how to use a loop for generating a numeric sequence efficiently.

---
+++ Basics/Alleven.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>   // include all standard libraries
+using namespace std;
+
+int main() {
+
+    // loop starts from 2 and runs till 100
+    // i increases by 2 each time (so only even numbers are printed)
+    for (int i = 2; i <= 100; i = i + 2) {
+        cout << i << endl;  // print the current even number
+    }
+
+    return 0; // end of program
+}
