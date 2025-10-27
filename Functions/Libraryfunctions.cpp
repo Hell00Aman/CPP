8e897e0b4db52fbc41f4fb010ee59b9d10f0247e
AMAN RAJ <amantk2205@gmail.com>
2025-10-28 00:29:58 +0530

    Create Libraryfunctions.cpp
    This C++ program demonstrates the use of mathematical functions from the <cmath> library. It calculates and displays the square root of 9 using sqrt(), the cube root of 8 using cbrt(), finds the minimum and maximum between two numbers using min() and max(), and computes powers using pow().

---
+++ Functions/Libraryfunctions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Square root of 9
+    cout << sqrt(9) << endl;
+    // Cube root of 8
+    cout << cbrt(8) << endl;
+    // Minimum of 3 and 8
+    cout << min(3,8) << endl;
+    // Maximum of 3 and 9
+    cout << max(3,9) << endl;
+    // 3 raised to power 3
+    cout << pow(3,3) << endl;
+}
