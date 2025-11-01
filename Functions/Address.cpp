4eb3c2ae459fca54cb6db566caf0133c490014ce
AMAN RAJ <amantk2205@gmail.com>
2025-11-01 19:31:52 +0530

    Create Address.cpp
    This C++ program demonstrates how to access and display the memory address of a variable. It declares an integer variable x with a value of 9 and uses the address-of operator (&) with cout to print the memory location where x is stored. The exact address output will vary each time the program runs.

---
+++ Functions/Address.cpp
@@ -0,0 +1,7 @@
+#include <iostream>      // Include input-output stream library
+using namespace std;
+
+int main() {
+    int x = 9;           // Declare an integer variable 'x' and assign it the value 9
+    cout << &x;          // Print the memory address of variable 'x' using the address-of operator '&'
+}
